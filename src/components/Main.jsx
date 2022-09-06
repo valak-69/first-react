502b0b1c7a9cb40b747a8acc086c8e52f349042b
valak-69 <valaknoob@gmail.com>
2022-09-06 18:53:01 +0530

    initial commit

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+            <ul className="main--facts">
+                <li>Was first released in 2013</li>
+                <li>Was originally created by Jordan Walke</li>
+                <li>Has well 100K stars on Github</li>
+                <li>is maintained by Facebook</li>
+                <li>Powers thousand of enterprise apps, including mobile apps</li>
+            </ul>
+        </main>
+    )
+}
